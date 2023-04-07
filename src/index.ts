4c58c92c31f59c5c2c7c209b0c430ab51e7c03f9
GillianLEVERT <gillian.levert@gmail.com>
2023-04-07 09:29:00 +0200

    Embeds discord

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,99 @@
 import { createSubgraphClient, gql } from "@colony/sdk/graph";
-import { pipe, subscribe } from 'wonka';
-import dotenv from 'dotenv'
+import { pipe, subscribe } from "wonka";
+const { EmbedBuilder } = require("discord.js");
+const Discord = require("discord.js");
+import dotenv from "dotenv";
 dotenv.config();
 
-
-const Discord = require('discord.js');
-const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds] });
+const client = new Discord.Client({
+  intents: [Discord.GatewayIntentBits.Guilds],
+});
 const TOKEN = process.env.TOKEN;
 
 const colonySubgraph = createSubgraphClient();
 
-colonySubgraph.subscription
-
-
+colonySubgraph.subscription;
 
 const QUERY = gql`
-subscription Subscription($orderBy: OneTxPayment_orderBy, $orderDirection: OrderDirection, $first: Int) {
-  oneTxPayments(orderBy: $orderBy, orderDirection: $orderDirection, first: $first) {
-    nPayouts
-    payment {
-      colony {
-        ensName
-        id
-      }
-      fundingPot {
-        fundingPotPayouts {
-          amount
+  subscription Subscription(
+    $orderBy: OneTxPayment_orderBy
+    $orderDirection: OrderDirection
+    $first: Int
+  ) {
+    oneTxPayments(
+      orderBy: $orderBy
+      orderDirection: $orderDirection
+      first: $first
+    ) {
+      nPayouts
+      payment {
+        colony {
+          ensName
+          id
+        }
+        fundingPot {
+          fundingPotPayouts {
+            amount
+          }
+        }
+        to
+        domain {
+          colonyAddress
         }
       }
-      to
-      domain {
-        colonyAddress
-      }
-     
-    
     }
-    
   }
-}`
+`;
 
 const VARIABLES = {
-  "orderDirection": "desc",
-  "orderBy": "timestamp",
-  "first": 1,
-}
+  orderDirection: "desc",
+  orderBy: "timestamp",
+  first: 1,
+};
 
-    console.log('New event :');
-    
-    const subscription = colonySubgraph.subscription(QUERY, VARIABLES);
-    pipe(
-      subscription,
-      subscribe(async (result: { data?: any; error?: any; }) => {
+console.log("New event :");
 
-        console.info("Last transaction :" + JSON.stringify(result.data));
-        await client.login(TOKEN),
-        client.once('ready', async () => {
-          
-          const chan = client.channels.cache.get('1034582332478337106');
-              await chan.send("Here de last transaction : \n"+"```json\n" +
-              JSON.stringify(result.data)
-            );
-        })
-      }),
-    );
+const subscription = colonySubgraph.subscription(QUERY, VARIABLES);
+pipe(
+  subscription,
+  subscribe(async (result: { data?: any; error?: any }) => {
+    console.info("Last transaction :" + JSON.stringify(result.data));
+    await client.login(TOKEN),
+      client.once("ready", async () => {
+        const chan = client.channels.cache.get("1034582332478337106");
+        const paymentInfo = result.data.oneTxPayments[0].payment;
+        const colonyName = `${paymentInfo.colony.ensName.split('.')[0]} Colony's`;
+        const fundingAmount = `${paymentInfo.fundingPot.fundingPotPayouts[0].amount}`;
+        const recipient = `${paymentInfo.to}`;
+        const colonyAddress = `${paymentInfo.domain.colonyAddress}`;
+
+        const message = new EmbedBuilder()
+          .setColor(5763719)
+          .setTitle("New incoming transaction !")
+          .setAuthor({
+            name: `${colonyName}`,
+          })
+          .addFields(
+            {
+              name: "From : ",
+              value: `${colonyAddress}`,
+              inline: true,
+            },
+            { name: "\u200B", value: "\u200B" },
+            {
+              name: "To : ",
+              value: `${recipient}`,
+              inline: true,
+            },
+            { name: "\u200B", value: "\u200B" },
+            {
+              name: "A total of :",
+              value: `${fundingAmount} Token`,
+              inline: true,
+            }
+          )
+          .setTimestamp();
+        await chan.send({ embeds: [message] });
+      });
+  })
+);
