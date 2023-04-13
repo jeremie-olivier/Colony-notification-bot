39487298aa153f549af8402509e93c084588b659
GillianLEVERT <gillian.levert@gmail.com>
2023-04-13 14:08:53 +0200

    new index

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const Discord = require("discord.js");
 import { ethers } from "ethers";
 import { providers } from "ethers";
 import dotenv from "dotenv";
+import { TypedDocumentNode } from "@graphql-typed-document-node/core";
+import { DocumentNode } from "graphql";
 dotenv.config();
 
 const client = new Discord.Client({
@@ -13,11 +15,58 @@ const client = new Discord.Client({
 });
 const TOKEN = process.env.TOKEN;
 
-const colonySubgraph = createSubgraphClient();
+async function run(): Promise<any> {
+await client.login(TOKEN)
+}
+run()
 
-colonySubgraph.subscription;
+function listenToColonyEvent(): void {
 
-const QUERY = gql`
+  const GQL = getGQLrequest() 
+  const GQLVARIABLES = getGqlVariables()
+  const subscription = getGqlSubscription(GQL, GQLVARIABLES)
+  pipe(
+    subscription,
+    subscribe(createAndSendMessage))
+  }
+  
+//when discord is ready
+client.once("ready", listenToColonyEvent)
+
+
+
+
+
+async function createAndSendMessage(): Promise<void> {
+  console.log(createAndSendMessage)
+   //const embed = getEmbed()
+    //const message = getDiscordMessage()
+    //const channel = getDiscordChannel()
+    //await channel.send(message);
+}
+
+
+
+
+function getGqlVariables(): any  {
+  const VARIABLES = {
+    orderDirection: "desc",
+    orderBy: "timestamp",
+    first: 1,
+  };
+return VARIABLES
+}
+
+function getGqlSubscription(gql: string | DocumentNode | TypedDocumentNode<any, any>, variables: any): any   {
+  const colonySubgraph = createSubgraphClient();
+  const subscription = colonySubgraph.subscription(gql, variables)
+  return subscription;
+
+
+}
+
+function getGQLrequest(): any   {
+  const QUERY = gql`
   subscription Subscription(
     $orderBy: OneTxPayment_orderBy
     $orderDirection: OrderDirection
@@ -51,81 +100,20 @@ const QUERY = gql`
     }
   }
 `;
+return QUERY
+}
 
-const VARIABLES = {
-  orderDirection: "desc",
-  orderBy: "timestamp",
-  first: 1,
-};
+function getEmbed() {
+  
+   
 
-console.log("New event :");
+}
+function getDiscordMessage() {
+    throw new Error("Function not implemented.")
+}
 
-const subscription = colonySubgraph.subscription(QUERY, VARIABLES);
-pipe(
-  subscription,
-  subscribe(async (result: { data?: any; error?: any }) => {
-    console.info("Last transaction :" + JSON.stringify(result.data));
-    await client.login(TOKEN),
-      client.once("ready", async () => {
-        const chan = client.channels.cache.get("1034582332478337106");
-        const paymentInfo = result.data.oneTxPayments[0].payment;
-        const colonyName = `${
-          paymentInfo.colony.ensName.split(".")[0]
-        } Colony's`;
-        const colonyTickers = paymentInfo.colony.token.symbol;
-        const domainName = paymentInfo.domain.name;
-        const fundingAmountWei =
-          paymentInfo.fundingPot.fundingPotPayouts[0].amount;
-        const fundingAmountEth = ethers.utils.formatEther(fundingAmountWei);
-        const truncatedAmount =
-          Math.floor(parseFloat(fundingAmountEth) * 100) / 100;
-        const recipient = paymentInfo.to;
-        const colonyAddress = paymentInfo.domain.colonyAddress;
-        const provider = new providers.JsonRpcProvider(
-          ColonyRpcEndpoint.Gnosis
-        );
-        const signer = new ethers.Wallet(
-          "55f32b12ca4ee3ce5157d40f42a8cb0171aa37e39600e2a906aca01e966275bc",
-          provider
-        );
-        const colonyNetwork = await ColonyNetwork.init(provider);
-        const recipientUsername = await colonyNetwork.getUsername(recipient);
+function getDiscordChannel() {
+  const channel = client.channels.cache.get("1034582332478337106");
+}
 
-        const embed = new EmbedBuilder()
-          .setColor(0x1cae9f)
-          .setTitle("New Payment")
-          .setDescription(
-            `${truncatedAmount} ${colonyTickers} has been payed to ${recipientUsername} ${recipient}`
-          )
-          .setThumbnail(
-            "https://cdn.discordapp.com/attachments/1087723564154749000/1095023300482191430/Forced.png"
-          )
-          .setAuthor({
-            name: `${colonyName}`,
-            url: "https://static-cdn.jtvnw.net/jtv_user_pictures/58a7369b-9a87-4c24-b8e0-99d71ff068ba-profile_image-70x70.png",
-          })
-          .addFields({ name: `In ${domainName} team.`, value: "\u200B" });
 
-        const message = {
-          content: "@business",
-          tts: false,
-          components: [
-            {
-              type: 1,
-              components: [
-                {
-                  style: 5,
-                  label: "View transaction on Colony",
-                  url: "https://xdai.colony.io/colony/chronodao/tx/0xa04eea13a88920facb23b9d305bcf8aadbcbaccda595a3c53d821be1a374df00",
-                  disabled: false,
-                  type: 2,
-                },
-              ],
-            },
-          ],
-          embeds: [embed],
-        };
-        await chan.send(message);
-      });
-  })
-);
